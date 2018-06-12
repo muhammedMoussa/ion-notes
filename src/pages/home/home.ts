e1834fb6f0e6f0cefa9e9d18e5c0f11482ce8f5b
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-06-12 21:10:46 +0200

    added add note page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AddNotePage } from '../add-note/add-note'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  addNote() {
+    this.navCtrl.push(AddNotePage)
+  }
 }
